f33589b82a41c0d6a6e28be250ef866a35e8e132
Taku Kobayashi <keep_slimbody@yahoo.co.jp>
2020-06-20 16:18:38 +0900

    classにしてvideoとかできるように

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,21 +1,43 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import { Button } from 'react-rainbow-components'
 
 import Page from '../components/Page'
 import Container from '../components/Container'
 import IndexLayout from '../layouts'
 
-const IndexPage = () => (
-  <IndexLayout>
-    <Page>
-      <Container>
-        <h1>Hi people</h1>
-        <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
-        <Link to="/page-2/">Go to page 2</Link>
-      </Container>
-    </Page>
-  </IndexLayout>
-)
+const Video = require('twilio-video')
+
+interface PageProps {
+}
+
+class IndexPage extends React.Component<PageProps> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  render():
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | string
+    | number
+    | {}
+    | React.ReactNodeArray
+    | React.ReactPortal
+    | boolean
+    | null
+    | undefined {
+    return (
+      <IndexLayout>
+        <Page>
+          <Container>
+            <Button shaded label="Button Brand" onClick={() => alert('clicked!')} variant="brand" className="rainbow-m-around_medium" />
+          </Container>
+        </Page>
+      </IndexLayout>
+    )
+  }
+}
 
 export default IndexPage
